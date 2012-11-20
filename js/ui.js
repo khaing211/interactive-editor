410bd3d4b79d015549224e31286580ab547eb94d
daifu <daifu.ye@gmail.com>
2012-11-19 19:08:45 -0800

    update the ui and fix bugs

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,7 @@
 /*jslint browser: true, regexp: true */
 /*global jQuery, $, IEditor, Format */
 
-var ieditor = new IEditor("editor","terminal", "system");
-
+var ieditor = IEditor("editor","terminal", "system");
 //Added scrollbar
 // Preloading the images used for the slider handle
 function createSlider(cachedObj, size) {
@@ -17,13 +16,9 @@ function createSlider(cachedObj, size) {
 
             if (curIndex > startIndex) {
                 // call forward
-                // console.log(curIndex);
-                // console.log('forwarding');
                 Format.forward(curIndex);
-            } else if(curIndex < startIndex) {
+                } else if(curIndex < startIndex) {
                 // call rewrind
-                // console.log(curIndex);
-                // console.log('rewinding');
                 Format.rewind(curIndex);
             }
         },
@@ -41,23 +36,37 @@ $(document).ready(function(){
         // startIndex = 100;
         createSlider(cachedObj, 1);
 
+        // push the console to the end of the page
+        var cachedConsole = $('#console');
+        cachedConsole.css({'position': 'fixed', 'bottom': 0, 'left': 0, 'z-index': 100});
+        // using waypoint to find out that div and change it css to fixed
+        var controlbarDiv = $('#controlbar');
+        controlbarDiv.waypoint(function(event, direction){
+            if (direction === 'down') {
+                controlbarDiv.css({'position': 'fixed', 'top': 5, 'z-index': 10});
+            } else {
+                controlbarDiv.css({'position': ''});
+            }
+        });
         // running debuging
         $("#debug").click(function(e){
             e.preventDefault();
             // Get the error
             var error = ieditor.error(),
             consoleCheck = $("#console");
-            //console.log(error);
             if (error === "") {
                 error = "Great! Successfully execute the script.";
             }
             consoleCheck.children('span').remove();
-            consoleCheck.append("<span>"+error+"<\/span>");
-            consoleCheck.modal();
+            consoleCheck.append("<span>"+error+"<\/span>").show().addClass('well');
+        });
+        $("#close-debug").click(function(e){
+            e.preventDefault();
+            $(this).parent().hide().removeClass('well').children('span').remove();
         });
 
         // // make the height to full size
         // var winHeight = $(window).height();
-        // $('.CodeMirror').height(winHeight - 60);
+        // $('.CodeMirror').height(winHeight - 120);
     })();
 });
